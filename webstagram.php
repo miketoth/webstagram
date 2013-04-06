8f378f76ae819b5435a7fcb370e776a3d46589d1
Greyson Parrelli <greyson.parrelli@live.com>
2013-04-06 17:46:36 -0400

    Flattened out file structure for extension purposes.

--- webstagram.php
+++ webstagram.php
@@ -38,8 +38,8 @@
     $specific = "";
         
     // Add all of our scripts and stylesheets
-    $returned_content = str_replace('</head>', script('js/libs/jquery.min.js') . script('js/libs/class.min.js') . script('js/libs/purl.js') .
-                                               $specific . script('js/'.$filter.'.js') . sheet('css/'.$filter.'.css') . '</head>', $returned_content);
+    $returned_content = str_replace('</head>', script('jquery.min.js') . script('class.min.js') . script('purl.js') .
+                                               $specific . script($filter.'.js') . sheet($filter.'.css') . '</head>', $returned_content);
     
     // Print the site to the DOM
     echo $returned_content;
@@ -70,16 +70,5 @@
     function sheet($path)
     {
         return '<link href="' . $path . '" rel="stylesheet" />';
-    }
-    
-    function cloudinary()
-    {
-        return script('js/libs/jquery.ui.widget.js') .
-               script('js/libs/canvas-to-blob.min.js') .
-               script('js/libs/jquery.cloudinary.js') .
-               script('js/libs/jquery.fileupload.js') .
-               script('js/libs/jquery.fileupload-fp.js') .
-               script('js/libs/jquery.iframe-transport.js') .
-               script('js/libs/load-image.min.js');
     }
 ?>
