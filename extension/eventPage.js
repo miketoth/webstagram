e8fac105ee2120674f31f6646f6754c6bfd70fd2
Greyson Parrelli <greyson.parrelli@live.com>
2013-04-06 20:04:19 -0400

    Worked on extension. Let's hope it works.

--- extension/eventPage.js
+++ extension/eventPage.js
@@ -1,70 +1,98 @@
 // ok this file needs a lot of work.
 
-    $(document).ready(function () {
-
+    $(document).ready(function () 
+    {
+        // Adding a class to the body so the plugins don't screw up our links
         $("body").addClass("stop-it");
 
+        // Loads proper filter on load
+        chrome.storage.local.get('filter', function(e)
+        {
+            var f = e['filter'];
+            
+            if (f == 'matrix')
+                loadFilter('matrix.css', 'matrix.js');
+                
+            else if (f == 'geocities')
+               loadFilter('geocities.css', 'geocities.js');
+            
+            else if (f == 'twenties')
+                loadFilter('twenties.css', 'twenties.js');
+            
+            else if (f == 'disco')
+                loadFilter('disco.css', 'disco.js');
+                
+        });
+
         // inject matrix javascript and CSS
-        $('#matrixStyle').click(function() {
-
-          //  location.reload(true);
-
-                // first CSS
-                chrome.tabs.insertCSS(null, {
-                    file : "matrix.css"
-                }, function() {
-
-                    // then javascript
-                    chrome.tabs.executeScript(null, { file : "matrix.js"
-                    }, function(){
-                        console.log("finished matrix");
-                    });
-                });
+        $('#matrixStyle').click(function() 
+        {
+            setFilter('matrix');
+            
+            // // first CSS
+            // chrome.tabs.insertCSS(null, { file : "matrix.css" }, function() 
+            // {
+            //     // then javascript
+            //     chrome.tabs.executeScript(null, { file : "matrix.js"}, function()
+            //     {
+            //         console.log("finished matrix");
+            //     });
+            // });
         });
 
         // repeat for geocities. This could be abstracted :(
-        $("#geocityStyle").click(function() {
-
-        //    location.reload(true);
-
-            chrome.tabs.insertCSS(null, {
-                file : "geocities.css"
-            }, function() {
-                chrome.tabs.executeScript(null, {
-                    file: "geocities.js"
-                }, function() {
-                        console.log("finished geocities");
-                });
-            });
+        $("#geocityStyle").click(function() 
+        {
+            setFilter('geocities');
+            // chrome.tabs.insertCSS(null, { file : "geocities.css" }, function() 
+            // {
+            //     chrome.tabs.executeScript(null, { file: "geocities.js" }, function()
+            //     {
+            //             console.log("finished geocities");
+            //     });
+            // });
         });
 
-        $("#twentiesStyle").click(function() {
-
-          //  location.reload(true);
-
-            chrome.tabs.insertCSS(null, {
-                file : "twenties.css"
-            }, function() {
-                chrome.tabs.executeScript(null, {
-                    file: "twenties.js"
-                }, function() {
-                    console.log("finished twenties");
-                });
-            });
+        $("#twentiesStyle").click(function() 
+        {
+            setFilter('twenties');
+            // chrome.tabs.insertCSS(null, { file : "twenties.css" }, function() 
+            // {
+            //     chrome.tabs.executeScript(null, { file: "twenties.js" }, function() 
+            //     {
+            //         console.log("finished twenties");
+            //     });
+            // });
         });
 
-        $("#discoStyle").click(function() {
-
-         //   location.reload(true);
-
-            chrome.tabs.insertCSS(null, {
-                file : "disco.css"
-            }, function() {
-                chrome.tabs.executeScript(null, {
-                    file: "disco.js"
-                }, function() {
-                    console.log("finished disco");
+        $("#discoStyle").click(function() 
+        {
+            setFilter('disco');
+            // chrome.tabs.insertCSS(null, { file : "disco.css" }, function() 
+            // {
+            //     chrome.tabs.executeScript(null, { file: "disco.js" }, function() 
+            //     {
+            //         console.log("finished disco");
+            //     });
+            // });
+        });
+        
+        function setFilter(f)
+        {
+            chrome.storage.local.set({'filter': f});
+            document.location.reload(true);
+        }
+        
+        function loadFilter(c, j)
+        {
+            // first CSS
+            chrome.tabs.insertCSS(null, { 'file' : c }, function() 
+            {
+                // then javascript
+                chrome.tabs.executeScript(null, { 'file' : j}, function()
+                {
+                    console.log("finished matrix");
                 });
             });
-        });
+        }
     });
