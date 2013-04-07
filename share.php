cd3eccbec6029ba815bd4e87b3f14754d03dee82
Greyson Parrelli <greyson.parrelli@live.com>
2013-04-07 15:17:40 -0400

    share.php debug

--- share.php
+++ share.php
@@ -3,7 +3,7 @@
     $url = $_GET['url'];
     $filter = $_GET['filter'];
 
-    $data = array("longUrl" => 'http://webstagramit.herokuapp.com/?filter='.$filter.'&url='. $url);
+    $data = array("longUrl" => 'http://webstagramit.herokuapp.com/webstagram.php?filter='.$filter.'&url='. $url);
     $data_string = json_encode($data);
 
     $ch = curl_init('https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyDYSc9qHIQcWRMyLcb429P6uY0Yhw1OdE0');
@@ -16,6 +16,7 @@
     );
 
     $result = curl_exec($ch);
-    echo $result
-   // echo json_decode($result, true)['id'];
+    echo $result;
+    $decoded = json_encode($result, true);
+    echo $decoded['id'];
 ?>
