889032c1f685668e3863c84e1298b43118e6db35
Greyson Parrelli <greyson.parrelli@live.com>
2013-04-07 16:22:13 -0400

    Updated our error page.

--- error.php
+++ error.php
@@ -1,7 +1,26 @@
 <!DOCTYPE html>
 
 <html>
+<head>
+    <title>Webstagram - Error</title>
+    <script src="http://use.edgefonts.net/lobster.js"></script>
+    <script src="http://use.edgefonts.net/ubuntu-condensed.js"></script>
+
+    <link href="index.css" rel="stylesheet" />
+</head>
+
 <body>
-    We're sorry, but this page does not seem to exist.
+    <div class="header-box">
+        <h1>Webstragram.</h1>
+        <h2>Fast beautiful link sharing</h2>
+    </div>
+
+    <div class="container">
+        <p>We're sorry, we can't Webstagram that page.</p>
+        <p>
+            <a href="http://webstagramit.herokuapp.com/">Return to Webstagram.</a>
+        </p>
+    </div>
+
 </body>
 </html>
